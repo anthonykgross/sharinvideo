3eb558304267871450266574be6d065991a2e2af
anthony <anthony.k.gross@gmail.com>
2018-12-12 12:17:10 +0100

    Remove Jwplayer

--- src/src/Sharing/VideoBundle/Resources/public/js/Compte/jwplayerMaster.js
+++ src/src/Sharing/VideoBundle/Resources/public/js/Compte/jwplayerMaster.js
@@ -1,44 +1,48 @@
 $(function(){
-    var urlVideo    = "https://www.youtube.com/watch?v=OiY3aZYJGTI";
-    var socket      = io.connect("https://"+document.domain+":2337");
+    var urlVideo    = "//www.youtube.com/watch?v=OiY3aZYJGTI";
+    var socket      = io.connect("http://"+document.domain+":2337");
     var delai       = 5;
-    var locked      = false;
-    
+    var player;
+
     $('#btn_valid_url').on('click', function(){
         urlVideo = $('#urlVideo').val();
         socket.emit('new_video', {url: urlVideo, master: master});
-        
-        jwplayer("mediaplayer").setup({
-            file: urlVideo,
-            width: "75%",
-            aspectratio: "16:9"
-        });
-        jwplayer("mediaplayer").onPause(function(d){
-            socket.emit('event_change', {master: master, data: d, event: 'onPause'});
-        });
-        jwplayer("mediaplayer").onPlay(function(d){
-            socket.emit('event_change', {master: master, data:{offset:jwplayer("mediaplayer").getPosition()}, event: 'onSeek'});
-            if(d.oldstate === "BUFFERING" && d.newstate === "PLAYING"){
-                 socket.emit('master_ready', {master: master});
-             }
-        });
-        jwplayer("mediaplayer").onBuffer(function(d){
-            socket.emit('event_change', {master: master, data: d, event: 'onBuffer'});
-        });
-        jwplayer("mediaplayer").onSeek(function(d){
-            socket.emit('event_change', {master: master, data: d, event: 'onSeek'});
-        });
-        jwplayer("mediaplayer").onTime(function(d){
-            socket.emit('time', {master:master, data: d, event: 'onTime', delai: delai});
-        });
-        jwplayer("mediaplayer").onBuffer(function(d){
-             if(d.oldstate === "PLAYING" && d.newstate === "BUFFERING"){
-                 socket.emit('master_wait', {master: master});
-             }
-        });
+        onYouTubeIframeAPIReady();
     });
     
     socket.on('message', function(j){
         $('#message').append($('<li/>').html(j));
     });
+
+
+    function onYouTubeIframeAPIReady() {
+        var elm = $('#mediaplayer');
+
+        player = new YT.Player(
+            elm.attr('id'), {
+                videoId: urlVideo,
+                events: {
+                    'onStateChange': onPlayerStateChange
+                }
+            }
+        );
+    }
+
+    function onPlayerStateChange(event) {
+        if(event.data === 1) {
+            socket.emit('event_change', {master: master, data: event, event: 'onPlay'});
+        }
+
+        if(event.data === 2) {
+            socket.emit('event_change', {master: master, data: event, event: 'onPause'});
+        }
+
+        if(event.data === 3) {
+            socket.emit('event_change', {master: master, data: event, event: 'onBuffer'});
+        }
+
+        if(event.data === 5) {
+            socket.emit('event_change', {master: master, data: event, event: 'onSeek'});
+        }
+    }
 });
