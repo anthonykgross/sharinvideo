e9d695bf7f1b15f4daa5b6b84d082ffa9348af56
anthony <anthony.k.gross@gmail.com>
2018-12-13 16:09:57 +0100

    Refactoring Websocket server

--- src/web/ServerChat.js
+++ src/web/ServerChat.js
@@ -1,111 +1,154 @@
-var     fs                  = require('fs'), 
-        http                = require('http'), 
-        socketio            = require('socket.io');
+var fs = require('fs'),
+    http = require('http'),
+    socketio = require('socket.io');
 
-var PORT_SOCKETIO_SERVER    = 2337;
+var PORT_SOCKETIO_SERVER = 2337;
 
-var server                  = http.createServer();
-var io                      = socketio.listen(server);
-var masters                 = [];
-var viewers                 = [];
-var sockets                 = [];
+var server = http.createServer();
+var io = socketio.listen(server);
 
-io.sockets.on('connection', function(socket) {
-    socket.on('init', function(j) {
-	console.log(j);
-        sockets[socket.id] = {
-            'type': 'viewer',
-            'master': j.master
-        };
-        
-        if(!viewers[j.master]){
-            viewers[j.master]=[];
+function Socket(type, name, ioSocket) {
+    this.ioSocket = ioSocket;
+    this.type = type;
+    this.name = name;
+}
+
+function Channel(master) {
+    this.master = master;
+    this.slaves = [];
+
+    /**
+     * @param socket Socket
+     * @returns {Channel}
+     */
+    this.addSlave = function(socket) {
+        this.slaves.push(socket);
+        return this;
+    };
+
+    this.emitMaster = function (event, data) {
+        this.master.ioSocket.emit(event, data);
+    };
+
+    this.emitSlaves= function (event, data) {
+        /**
+         * @var socket Socket
+         */
+        this.slaves.forEach(function (socket) {
+            socket.ioSocket.emit(event, data);
+        });
+    };
+
+    this.emitAll = function (event, data) {
+        this.emitMaster(event, data);
+        this.emitSlaves(event, data);
+    };
+}
+
+var ChannelList = {
+    /**
+     * @var channels Channel[]
+     */
+    channels: [],
+
+    /**
+     * @param name
+     * @param channel
+     * @returns {ChannelList}
+     */
+    push: function (name, channel) {
+        this.channels[name] = channel;
+        return this;
+    },
+
+    /**
+     * @param name
+     * @returns Channel | null
+     */
+    getChannelByMasterName: function(name) {
+        return this.channels[name];
+    },
+
+    /**
+     * @param name
+     * @returns {Boolean}
+     */
+    masterConnected: function(name){
+        var channel = (this.getChannelByMasterName(name));
+        var ok = (channel);
+
+        if (ok) {
+            ok = (channel.master);
         }
-        viewers[j.master].push(socket);
-        if(!masters[j.master]){
-            socket.emit('message', j.master+" ne partage pas de vidéo actuellement.");
-        }
-        else{
-            masters[j.master]['socket'].emit('message',"Le client #"+socket.id+" vient de se connecter.");
-            socket.emit('new_video',{url:masters[j.master]['video']});
-            socket.emit('event_change',masters[j.master]['event']);
-        }
-    });
-    socket.on('master_wait', function(j) {
-        if(viewers[j.master]){
-            viewers[j.master].forEach(function(sock){
-                sock.emit('message', j.master+" rencontre quelques ralentissements.");
-                socket.emit('event_change',{event:'onPause'});
-            });
-        }
-    });
-    socket.on('master_ready', function(j) {
-        if(viewers[j.master]){
-            viewers[j.master].forEach(function(sock){
-                sock.emit('message', j.master+" reprend la vidéo.");
-            });
-        }
-    });
-    socket.on('client_wait', function(j) {
-        masters[j.master]['socket'].emit('message',"Le client #"+socket.id+" rencontre quelques ralentissements.");
-    });
-    socket.on('client_ready', function(j) {
-        masters[j.master]['socket'].emit('message',"Le client #"+socket.id+" reprend la vidéo.");
-    });
-    socket.on('new_video', function(j) {
-        sockets[socket.id] = {
-            'type': 'master',
-            'master': j.master
-        };
-        masters[j.master] = {
-            'socket': socket,
-            'second': 0,
-            'video':j.url
-        };
-        if(viewers[j.master]){
-            viewers[j.master].forEach(function(sock){
-                sock.emit('message', j.master+" partage une nouvelle video.");
-                sock.emit('new_video',j);
-            });
-        }
-    });
-    socket.on('event_change', function(j) {
-        masters[j.master]['event'] = j;
-        if(viewers[j.master]){
-            viewers[j.master].forEach(function(sock){
-                sock.emit('event_change',j);
-            });
-        }
-    });
-    socket.on('time', function(j) {
-        if (masters[j.master]) {
-            masters[j.master]['second'] = j.data.position;
-        }
-        if(viewers[j.master]){
-            viewers[j.master].forEach(function(sock){
-                sock.emit('event_change',j);
-            });
-        }
-    });
-    socket.on('disconnect', function() {
-        if(sockets[socket.id]){
-            console.log(sockets[socket.id]);
-            if(sockets[socket.id]['type'] === 'viewer'){
-                if(masters[sockets[socket.id]['master']]){
-                    masters[sockets[socket.id]['master']]['socket'].emit('message',"Le client #"+socket.id+" s'est déconnecté.");
+        return ok;
+    },
+
+    /**
+     * @param iosocketId
+     * @returns Channel | null
+     */
+    getChannelByIOSocket: function(iosocketId) {
+        var c;
+
+        Object.keys(this.channels).forEach((name) => {
+            var channel = this.channels[name];
+            var master = channel.master;
+
+            if (master) {
+                if (iosocketId === master.ioSocket.id) {
+                    c = channel;
                 }
-                delete viewers[sockets[socket.id]['master']];
             }
-            else{
-                if(viewers[sockets[socket.id]['master']]){
-                    viewers[sockets[socket.id]['master']].forEach(function(sock){
-                        sock.emit('message',sockets[socket.id]['master']+" s'est déconnecté.");
-                    });
+
+            channel.slaves.forEach((socket) => {
+                if (iosocketId === socket.ioSocket.id) {
+                    c = socket;
                 }
-                delete masters[sockets[socket.id]['master']];
-            }
-            delete sockets[socket.id];
+            });
+        });
+        return c;
+    },
+
+    /**
+     * @param name
+     * @returns {Channel}
+     */
+    createChannel: function(name) {
+        var channel = ChannelList.getChannelByMasterName(name);
+        if (!channel) {
+            channel = new Channel();
+            ChannelList.push(name, channel);
         }
+        return channel;
+    }
+};
+
+io.sockets.on('connection', function (socket) {
+    socket.on('slave_ready', function (d) {
+        var channel = ChannelList.createChannel(d.master);
+        var slave = new Socket('slave', null, socket);
+        channel.addSlave(slave);
+    });
+
+    socket.on('master_ready', function (d) {
+        var channel = ChannelList.createChannel(d.master);
+        channel.master = new Socket('master', d.master, socket);
+    });
+
+    socket.on('slave_wait', function (d) {
+        console.log(d);
+    });
+
+    socket.on('event_change', function (d) {
+        console.log(d);
+        var channel = ChannelList.getChannelByMasterName(d.master);
+        if (channel) {
+            channel.emitAll('event_change', d);
+        }
+    });
+
+    socket.on('disconnect', function () {
+        var channel = ChannelList.getChannelByIOSocket(socket.id);
     });
 });
 server.listen(PORT_SOCKETIO_SERVER);
